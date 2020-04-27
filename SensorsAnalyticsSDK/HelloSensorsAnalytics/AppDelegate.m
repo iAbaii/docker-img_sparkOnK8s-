e3af99ef9a9519337157383ad8d26eab9d75a8a0
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-04-27 12:00:06 +0000

    fix doctest

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
@@ -2,4 +2,19 @@
 //  AppDelegate.m
 //  HelloSensorsAnalytics
 //
-//  Created
+//  Created by 曹犟 on 15/7/4.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import "SensorsAnalyticsSDK.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)applic

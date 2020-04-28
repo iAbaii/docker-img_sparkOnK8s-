41aa4752e1ffc840bd331ce82bf161559783b157
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-04-28 08:11:49 +0000

    Fixed files

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
@@ -19,4 +19,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [SensorsAnalyticsSDK sharedInstanceWithServerURL:@"http://test-zouyuhan.cloud.sensorsdata.cn:8006/sa?project=wangzhuozhou&token=db52d13749514676"
- 
+                                     andConfigureURL:@"http://test-zouyuhan.cloud.sensorsdata.cn:8006/config/?project=wangzhuozhou"
+                                        andDebugMode:SensorsAnalyticsDebugAndTrack];
+//  

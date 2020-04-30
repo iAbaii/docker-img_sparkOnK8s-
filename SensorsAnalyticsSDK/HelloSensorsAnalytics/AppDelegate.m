eb9356866ba78ab43c419134ee69e447629396f3
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-04-30 22:23:28 +0000

    Index update

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
@@ -42,4 +42,5 @@
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackgroun
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state

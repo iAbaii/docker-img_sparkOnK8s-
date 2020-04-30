718cbeecbb2532039f59d7ce3ddabcf5b31c2a63
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-04-30 08:01:24 +0000

    HID: uhid: add UHID_CREATE and UHID_DESTROY events

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
@@ -38,4 +38,4 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is a
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins t

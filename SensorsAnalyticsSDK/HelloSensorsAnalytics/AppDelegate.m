b832ef718e2d90e806b6b3f71a5153923f0cd15f
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-03 11:33:41 +0000

    content

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AppDelegate.m
@@ -56,4 +56,7 @@
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackg
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end

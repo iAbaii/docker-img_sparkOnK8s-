04aac789918e47f151443ec6b199ad20d046b833
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-04 21:54:52 +0000

    Cleaned up.

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AutoTrackViewController.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AutoTrackViewController.m
@@ -2,4 +2,16 @@
 //  AutoTrackViewController.m
 //  SensorsAnalyticsSDK
 //
-//  Created by 王灼
+//  Created by 王灼洲 on 2017/4/27.
+//  Copyright © 2017年 SensorsData. All rights reserved.
+//
+
+#import "AutoTrackViewController.h"
+
+@interface AutoTrackViewController ()
+
+@end
+
+@implementation AutoTrackViewController
+
+- (void)viewDidL

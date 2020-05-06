f1cb71a509e0ca634c93adf7110276c8f63fa089
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-06 10:31:29 +0000

    Translated using Weblate (West Flemish)

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/AutoTrackViewController.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/AutoTrackViewController.m
@@ -47,4 +47,13 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the n
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)onButton1Click:(id)sender {
+}
+- (IBAction)segmentOnClick:(id)sender {
+}
+@end

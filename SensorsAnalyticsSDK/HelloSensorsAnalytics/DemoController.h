fd960f6fb28686411c66ea30b47356d90395789b
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-08 14:55:34 +0000

    Delete 1.png

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/DemoController.h
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/DemoController.h
@@ -4,3 +4,17 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by ZouYuhan on 1/19/16.
+//  Copyright © 2016 SensorsData. All rights reserved.
+//
+
+#ifndef DemoController_h
+#define DemoController_h
+
+#import <UIKit/UIKit.h>
+#import "SensorsAnalyticsSDK.h"
+
+@interface DemoController : UITableViewController<SAScreenAutoTracker, SAUIViewAutoTrackDelegate, UIActionSheetDelegate>
+
+@end
+
+#endif /* DemoController_h */

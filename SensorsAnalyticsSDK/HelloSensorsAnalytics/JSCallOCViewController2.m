56988ee745604ffe4f8e3f4f46929b89e15257af
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-18 11:37:29 +0000

    fixed sidebar

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/JSCallOCViewController2.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/JSCallOCViewController2.m
@@ -6,4 +6,11 @@
 //  Copyright © 2016年 SensorsData. All rights reserved.
 //
 
-#import "JSCallOCV
+#import "JSCallOCViewController2.h"
+#import "SensorsAnalyticsSDK.h"
+@import WebKit;
+
+@interface JSCallOCViewController2 ()<WKNavigationDelegate, WKUIDelegate>
+@property WKWebView *webView;
+@end
+@implementation JSCallOCViewContr

09a4491e3d3d3592886c881a5f6f9becae0efaf8
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-22 14:51:14 +0000

    Fix more tests

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/ViewController.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/ViewController.m
@@ -1,14 +1,14 @@
+
 //
-//  ViewController.h
+//  ViewController.m
 //  HelloSensorsAnalytics
 //
 //  Created by 曹犟 on 15/7/4.
 //  Copyright (c) 2015年 SensorsData. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "ViewController.h"
 
-@interface ViewController : UITableViewController
+@implementation ViewController
 
 @end
-
---
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/main.m
@@ -0,0 +1,6 @@
+//
+//  main.m
+//  HelloSensorsAnalytics
+//
+//  Created by 曹犟 on 15/7/4.
+//  Copyright (c) 2015年 SensorsDa

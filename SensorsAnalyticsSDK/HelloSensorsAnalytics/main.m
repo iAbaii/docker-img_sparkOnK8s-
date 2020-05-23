a80653beb6639d06d52f996d32fa20a28da1e95d
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-23 19:16:56 +0000

    README.md edited

--- SensorsAnalyticsSDK/HelloSensorsAnalytics/main.m
+++ SensorsAnalyticsSDK/HelloSensorsAnalytics/main.m
@@ -3,4 +3,12 @@
 //  HelloSensorsAnalytics
 //
 //  Created by 曹犟 on 15/7/4.
-//  Copyright (c) 2015年 SensorsDa
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppD

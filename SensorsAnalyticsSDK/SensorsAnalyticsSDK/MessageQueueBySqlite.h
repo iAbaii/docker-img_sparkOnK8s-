7eaf0eaefbabb4c2c1d76b31cf2e61fb04cf6ca2
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-27 10:06:04 +0000

    Update cuda-java-truecrack-python-pip-install.bash

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/MessageQueueBySqlite.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/MessageQueueBySqlite.h
@@ -3,4 +3,22 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 曹犟 on 15/7/7.
-//  Copyright (c) 2015年 SensorsData. All rights reserv
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  @abstract
+ *  一个基于Sqlite封装的接口，用于向其中添加和获取数据
+ */
+@interface MessageQueueBySqlite : NSObject
+
+
+/**
+ *  @abstract
+ *  根据传入的文件路径初始化
+ *
+ *  @param filePath 传入的数据文件路径
+ *
+ *  @

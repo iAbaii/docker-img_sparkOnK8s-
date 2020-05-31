60df322700829270672cfce0a269b3c30beec0f9
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-05-31 13:48:14 +0000

    1.19.1

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSData+SABase64.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSData+SABase64.h
@@ -3,4 +3,8 @@
 //  base64
 //
 //  Created by Matt Gallagher on 2009/06/03.
-//  Copyright 2009 Matt Gallagher. All rights reserve
+//  Copyright 2009 Matt Gallagher. All rights reserved.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part

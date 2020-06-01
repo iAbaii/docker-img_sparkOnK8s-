c3385cebf545818bee2af9c933c479c2813f73ac
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-06-01 08:19:16 +0000

    SVN_SILENT compile

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSData+SABase64.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSData+SABase64.h
@@ -7,4 +7,11 @@
 //
 //  Permission is given to use this source code file, free of charge, in any
 //  project, commercial or otherwise, entirely at your risk, with the condition
-//  that any redistribution (in part
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#import <Foundation/Foundation.h>
+
+void *SA_NewBas

f44c5d86836b34be43d5b11cb9252c255b441560
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-06-04 06:40:40 +0000

    1.0.0-beta.3

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSInvocation+SAHelpers.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/NSInvocation+SAHelpers.m
@@ -1,2 +1,12 @@
 //
-//  Copyright (c) 2016年 SensorsData. All rights 
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+/// Copyright (c) 2014 Mixpanel. All rights reserved.
+
+#import <objc/runtime.h>
+#import "SALogger.h"
+#import "NSInvocation+SAHelpers.h"
+
+typedef union {
+    char                    _chr;
+    unsigned char        

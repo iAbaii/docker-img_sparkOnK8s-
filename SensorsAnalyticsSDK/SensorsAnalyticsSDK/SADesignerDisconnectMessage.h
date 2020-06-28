f44b3d66000ba193575f6c64c91d80d3bd93f23b
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-06-28 16:49:58 +0000

    fix misprint

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SADesignerDisconnectMessage.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SADesignerDisconnectMessage.h
@@ -3,4 +3,14 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 雨晗 on 1/28/16.
-//  Copyright (c) 2016年 SensorsData
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+
+#ifndef SADesignerDisconnectMessage_h
+#define SADesignerDisconnectMessage_h
+
+#import "SAAbstractDesignerMessage.h"
+
+extern NSString *const SADesignerDisconnectMessageType;
+
+@interface SADe

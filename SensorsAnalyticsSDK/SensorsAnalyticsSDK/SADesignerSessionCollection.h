ad8213b87a3733db605db96ab8e89cdf1ebb2a46
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-06-29 20:22:41 +0000

    Schema updates

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SADesignerSessionCollection.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SADesignerSessionCollection.h
@@ -3,4 +3,16 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 雨晗 on 1/18/16.
-//  Copyright (c) 2016年 SensorsData. All 
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+///  Created by Amanda Canyon on 8/22/14.
+///  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SADesignerSessionCollection <NSObject>
+
+- (void)cleanup;
+
+@en

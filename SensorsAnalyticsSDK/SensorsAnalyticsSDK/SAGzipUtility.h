5ad0e9c12924b12b81bbb9172f150e1ae0b24e03
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-09 11:48:50 +0000

    MOD:

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAGzipUtility.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAGzipUtility.h
@@ -19,4 +19,8 @@
  Uses zlib to compress the given data. Note that gzip headers will be added so
  that the data can be easily decompressed using a tool like WinZip, gunzip, etc.
  
- Note: Special thanks 
+ Note: Special thanks to Robbie Hanson of Deusty Designs for sharing sample code
+ showing how deflateInit2() can be used to make zlib generate a compressed file
+ with gzip headers:
+ 
+ http://deusty.blogspot.com/2007/07/gzip-compressiondeco

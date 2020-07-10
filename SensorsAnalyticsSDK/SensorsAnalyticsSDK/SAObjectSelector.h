e1dacc08e6500fc912af9564729727190fa8e7ce
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-10 19:46:37 +0000

    macvtap: signal truncated packets

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSelector.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSelector.h
@@ -1,3 +1,12 @@
 //
 //  ObjectSelector.h
-//  SensorsAna
+//  SensorsAnalyticsSDK
+//
+//  Created by 雨晗 on 1/20/16
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+///  Created by Alex Hofsteede on 5/5/14.
+///  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#imp

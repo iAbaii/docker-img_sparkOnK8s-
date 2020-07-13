7d87753eab7b284fd7ca854fe8e4b922c7d3d2e9
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-13 12:21:24 +0000

    Fix folder structure

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSelector.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSelector.m
@@ -118,3 +118,225 @@
  */
 
 - (BOOL)isLeafSelected:(id)leaf fromRoot:(id)root {
+    return [self isLeafSelected:leaf fromRoot:root evaluatingFinalPredicate:YES];
+}
+
+- (BOOL)fuzzyIsLeafSelected:(id)leaf fromRoot:(id)root {
+    return [self isLeafSelected:leaf fromRoot:root evaluatingFinalPredicate:NO];
+}
+
+- (BOOL)isLeafSelected:(id)leaf fromRoot:(id)root evaluatingFinalPredicate:(BOOL)finalPredicate {
+    BOOL isSelected = YES;
+    NSArray *views = @[leaf];
+    NSUInteger n = [_filters count], i = n;
+    while(i--) {
+        SAObjectFilter *filter = _filters[i];
+        filter.nameOnly = (i == n-1 && !finalPredicate);
+        if (![filter appliesToAny:views]) {
+            isSelected = NO;
+            break;
+        }
+        views = [filter applyReverse:views];
+        if ([views count] == 0) {
+            break;
+        }
+    }
+    return isSelected && [views indexOfObject:root] != NSNotFound;
+}
+
+- (SAObjectFilter *)nextFilter {
+    SAObjectFilter *filter;
+    if ([_scanner scanCharactersFromSet:_separatorChars intoString:nil]) {
+        NSString *name;
+        filter = [[SAObjectFilter alloc] init];
+        if ([_scanner scanCharactersFromSet:_classAndPropertyChars intoString:&name]) {
+            filter.name = name;
+        } else {
+            filter.name = @"*";
+        }
+        if ([_scanner scanCharactersFromSet:_flagStartChar intoString:nil]) {
+            NSString *flags;
+            [_scanner scanUpToCharactersFromSet:_flagEndChar intoString:&flags];
+            for (NSString *flag in[flags componentsSeparatedByString:@"|"]) {
+                if ([flag isEqualToString:@"unique"]) {
+                    filter.unique = YES;
+                }
+            }
+        }
+        if ([_scanner scanCharactersFromSet:_predicateStartChar intoString:nil]) {
+            NSString *predicateFormat;
+            NSInteger index = 0;
+            if ([_scanner scanInteger:&index] && [_scanner scanCharactersFromSet:_predicateEndChar intoString:nil]) {
+                filter.index = @((NSUInteger)index);
+            } else {
+                [_scanner scanUpToCharactersFromSet:_predicateEndChar intoString:&predicateFormat];
+                @try {
+                    NSPredicate *parsedPredicate = [NSPredicate predicateWithFormat:predicateFormat];
+                    filter.predicate = [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings) {
+                        @try {
+                            return [parsedPredicate evaluateWithObject:evaluatedObject substitutionVariables:bindings];
+                        }
+                        @catch (NSException *exception) {
+                            return false;
+                        }
+                    }];
+                }
+                @catch (NSException *exception) {
+                    filter.predicate = [NSPredicate predicateWithValue:NO];
+                }
+
+                [_scanner scanCharactersFromSet:_predicateEndChar intoString:nil];
+            }
+        }
+    }
+    return filter;
+}
+
+- (Class)selectedClass {
+    if ([_filters count] > 0) {
+        return NSClassFromString(((SAObjectFilter *)_filters[[_filters count] - 1]).name);
+    }
+    return nil;
+}
+
+- (NSString *)description {
+    return self.string;
+}
+
+@end
+
+@implementation SAObjectFilter
+
+- (instancetype)init {
+    if((self = [super init])) {
+        self.unique = NO;
+        self.nameOnly = NO;
+    }
+    return self;
+}
+
+/*
+ Apply this filter to the views, returning all of their children
+ that match this filter's class / predicate pattern
+ */
+- (NSArray *)apply:(NSArray *)views {
+    NSMutableArray *result = [NSMutableArray array];
+    Class class = NSClassFromString(_name);
+    if (class || [_name isEqualToString:@"*"]) {
+        // Select all children
+        for (NSObject *view in views) {
+            NSArray *children = [self getChildrenOfObject:view ofType:class];
+            if (_index && [_index unsignedIntegerValue] < [children count]) {
+                // Indexing can only be used for subviews of UIView
+                if ([view isKindOfClass:[UIView class]]) {
+                    children = @[children[[_index unsignedIntegerValue]]];
+                } else {
+                    children = @[];
+                }
+            }
+            [result addObjectsFromArray:children];
+        }
+    }
+
+    if (!self.nameOnly) {
+        // If unique is set and there are more than one, return nothing
+        if(self.unique && [result count] != 1) {
+            return @[];
+        }
+        // Filter any resulting views by predicate
+        if (self.predicate) {
+            return [result filteredArrayUsingPredicate:self.predicate];
+        }
+    }
+    return [result copy];
+}
+
+/*
+ Apply this filter to the views. For any view that
+ matches this filter's class / predicate pattern, return
+ its parents.
+ */
+- (NSArray *)applyReverse:(NSArray *)views {
+    NSMutableArray *result = [NSMutableArray array];
+    for (NSObject *view in views) {
+        if ([self appliesTo:view]) {
+            [result addObjectsFromArray:[self getParentsOfObject:view]];
+        }
+    }
+    return [result copy];
+}
+
+/*
+ Returns whether the given view would pass this filter.
+ */
+- (BOOL)appliesTo:(NSObject *)view {
+    return (([self.name isEqualToString:@"*"] || [view isKindOfClass:NSClassFromString(self.name)])
+            && (self.nameOnly || (
+                (!self.predicate || [_predicate evaluateWithObject:view])
+                && (!self.index || [self isView:view siblingNumber:[_index integerValue]])
+                && (!(self.unique) || [self isView:view oneOfNSiblings:1])))
+            );
+}
+
+/*
+ Returns whether any of the given views would pass this filter
+ */
+- (BOOL)appliesToAny:(NSArray *)views {
+    for (NSObject *view in views) {
+        if ([self appliesTo:view]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+/*
+ Returns true if the given view is at the index given by number in
+ its parent's subviews. The view's parent must be of type UIView
+ */
+
+- (BOOL)isView:(NSObject *)view siblingNumber:(NSInteger)number {
+    return [self isView:view siblingNumber:number of:-1];
+}
+
+- (BOOL)isView:(NSObject *)view oneOfNSiblings:(NSInteger)number {
+    return [self isView:view siblingNumber:-1 of:number];
+}
+
+- (BOOL)isView:(NSObject *)view siblingNumber:(NSInteger)index of:(NSInteger)numSiblings {
+    NSArray *parents = [self getParentsOfObject:view];
+    for (NSObject *parent in parents) {
+        if ([parent isKindOfClass:[UIView class]]) {
+            NSArray *siblings = [self getChildrenOfObject:parent ofType:NSClassFromString(_name)];
+            if ((index < 0 || ((NSUInteger)index < [siblings count] && siblings[(NSUInteger)index] == view))
+                && (numSiblings < 0 || [siblings count] == (NSUInteger)numSiblings)) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+- (NSArray *)getParentsOfObject:(NSObject *)obj {
+    NSMutableArray *result = [NSMutableArray array];
+    if ([obj isKindOfClass:[UIView class]]) {
+        if ([(UIView *)obj superview]) {
+            [result addObject:[(UIView *)obj superview]];
+        }
+        // For UIView, nextResponder should be its controller or its superview.
+        if ([(UIView *)obj nextResponder] && [(UIView *)obj nextResponder] != [(UIView *)obj superview]) {
+            [result addObject:[(UIView *)obj nextResponder]];
+        }
+    } else if ([obj isKindOfClass:[UIViewController class]]) {
+        if ([(UIViewController *)obj parentViewController]) {
+            [result addObject:[(UIViewController *)obj parentViewController]];
+        }
+        if ([(UIViewController *)obj presentingViewController]) {
+            [result addObject:[(UIViewController *)obj presentingViewController]];
+        }
+        if ([UIApplication sharedApplication].keyWindow.rootViewController == obj) {
+            //TODO is there a better way to get the actual window that has this VC
+            [result addObject:[UIApplication sharedApplication].keyWindow];
+        }
+    }
+    return [result copy];

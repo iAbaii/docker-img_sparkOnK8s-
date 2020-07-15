61018933ab23faaa68e18142e9a7052513986039
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-15 15:06:51 +0000

    header updates

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSerializer.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAObjectSerializer.m
@@ -133,3 +133,191 @@
 
 - (NSArray *)parameterVariationsForPropertySelector:(SAPropertySelectorDescription *)selectorDescription {
     NSAssert([selectorDescription.parameters count] <= 1, @"Currently only support selectors that take 0 to 1 arguments.");
+
+    NSMutableArray *variations = [[NSMutableArray alloc] init];
+
+    // TODO: write an algorithm that generates all the variations of parameter combinations.
+    if ([selectorDescription.parameters count] > 0) {
+        SAPropertySelectorParameterDescription *parameterDescription = (selectorDescription.parameters)[0];
+        for (id value in [self allValuesForType:parameterDescription.type]) {
+            [variations addObject:@[ value ]];
+        }
+    } else {
+        // An empty array of parameters (for methods that have no parameters).
+        [variations addObject:@[]];
+    }
+
+    return [variations copy];
+}
+
+- (id)instanceVariableValueForObject:(id)object
+                 propertyDescription:(SAPropertyDescription *)propertyDescription {
+    NSParameterAssert(object != nil);
+    NSParameterAssert(propertyDescription != nil);
+
+    Ivar ivar = class_getInstanceVariable([object class], [propertyDescription.name UTF8String]);
+    if (ivar) {
+        const char *objCType = ivar_getTypeEncoding(ivar);
+
+        ptrdiff_t ivarOffset = ivar_getOffset(ivar);
+        const void *objectBaseAddress = (__bridge const void *)object;
+        const void *ivarAddress = (((const uint8_t *)objectBaseAddress) + ivarOffset);
+
+        switch (objCType[0])
+        {
+            case _C_ID:       return object_getIvar(object, ivar);
+            case _C_CHR:      return @(*((char *)ivarAddress));
+            case _C_UCHR:     return @(*((unsigned char *)ivarAddress));
+            case _C_SHT:      return @(*((short *)ivarAddress));
+            case _C_USHT:     return @(*((unsigned short *)ivarAddress));
+            case _C_INT:      return @(*((int *)ivarAddress));
+            case _C_UINT:     return @(*((unsigned int *)ivarAddress));
+            case _C_LNG:      return @(*((long *)ivarAddress));
+            case _C_ULNG:     return @(*((unsigned long *)ivarAddress));
+            case _C_LNG_LNG:  return @(*((long long *)ivarAddress));
+            case _C_ULNG_LNG: return @(*((unsigned long long *)ivarAddress));
+            case _C_FLT:      return @(*((float *)ivarAddress));
+            case _C_DBL:      return @(*((double *)ivarAddress));
+            case _C_BOOL:     return @(*((_Bool *)ivarAddress));
+            case _C_SEL:      return NSStringFromSelector(*((SEL*)ivarAddress));
+            default:
+                NSAssert(NO, @"Currently unsupported return type!");
+                break;
+        }
+    }
+
+    return nil;
+}
+
+- (NSInvocation *)invocationForObject:(id)object
+              withSelectorDescription:(SAPropertySelectorDescription *)selectorDescription {
+    NSUInteger __unused parameterCount = [selectorDescription.parameters count];
+
+    SEL aSelector = NSSelectorFromString(selectorDescription.selectorName);
+    NSAssert(aSelector != nil, @"Expected non-nil selector!");
+
+    NSMethodSignature *methodSignature = [object methodSignatureForSelector:aSelector];
+    NSInvocation *invocation = nil;
+
+    if (methodSignature) {
+        NSAssert([methodSignature numberOfArguments] == (parameterCount + 2), @"Unexpected number of arguments!");
+
+        invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+        invocation.selector = aSelector;
+    }
+    return invocation;
+}
+
+- (id)propertyValue:(id)propertyValue
+propertyDescription:(SAPropertyDescription *)propertyDescription
+            context:(SAObjectSerializerContext *)context {
+    if (propertyValue != nil) {
+        if ([context isVisitedObject:propertyValue]) {
+            return [_objectIdentityProvider identifierForObject:propertyValue];
+        }
+        else if ([self isNestedObjectType:propertyDescription.type])
+        {
+            [context enqueueUnvisitedObject:propertyValue];
+            return [_objectIdentityProvider identifierForObject:propertyValue];
+        }
+        else if ([propertyValue isKindOfClass:[NSArray class]] || [propertyValue isKindOfClass:[NSSet class]])
+        {
+            NSMutableArray *arrayOfIdentifiers = [[NSMutableArray alloc] init];
+            for (id value in propertyValue) {
+                if ([context isVisitedObject:value] == NO) {
+                    [context enqueueUnvisitedObject:value];
+                }
+
+                [arrayOfIdentifiers addObject:[_objectIdentityProvider identifierForObject:value]];
+            }
+            propertyValue = [arrayOfIdentifiers copy];
+        }
+    }
+
+    return [propertyDescription.valueTransformer transformedValue:propertyValue];
+}
+
+- (id)propertyValueForObject:(NSObject *)object
+     withPropertyDescription:(SAPropertyDescription *)propertyDescription
+                     context:(SAObjectSerializerContext *)context {
+    NSMutableArray *values = [[NSMutableArray alloc] init];
+
+
+    SAPropertySelectorDescription *selectorDescription = propertyDescription.getSelectorDescription;
+
+    if (propertyDescription.useKeyValueCoding) {
+        // the "fast" (also also simple) path is to use KVC
+        id valueForKey = [object valueForKey:selectorDescription.selectorName];
+
+        id value = [self propertyValue:valueForKey
+                   propertyDescription:propertyDescription
+                               context:context];
+
+        NSDictionary *valueDictionary = @{
+                @"value" : (value ?: [NSNull null])
+        };
+
+        [values addObject:valueDictionary];
+    } else if (propertyDescription.useInstanceVariableAccess) {
+        id valueForIvar = [self instanceVariableValueForObject:object propertyDescription:propertyDescription];
+
+        id value = [self propertyValue:valueForIvar
+                   propertyDescription:propertyDescription
+                               context:context];
+
+        NSDictionary *valueDictionary = @{
+            @"value" : (value ?: [NSNull null])
+        };
+
+        [values addObject:valueDictionary];
+    } else {
+        // the "slow" NSInvocation path. Required in order to invoke methods that take parameters.
+        NSInvocation *invocation = [self invocationForObject:object withSelectorDescription:selectorDescription];
+        if (invocation) {
+            NSArray *parameterVariations = [self parameterVariationsForPropertySelector:selectorDescription];
+            
+            for (NSArray *parameters in parameterVariations) {
+                [invocation sa_setArgumentsFromArray:parameters];
+                [invocation invokeWithTarget:object];
+                
+                id returnValue = [invocation sa_returnValue];
+                
+                id value = [self propertyValue:returnValue
+                           propertyDescription:propertyDescription
+                                       context:context];
+
+                NSDictionary *valueDictionary = @{
+                                                  @"where": @{ @"parameters" : parameters },
+                                                  @"value": (value ?: [NSNull null])
+                                                  };
+                
+                [values addObject:valueDictionary];
+            }
+        }
+    }
+    
+
+    return @{@"values": values};
+}
+
+- (BOOL)isNestedObjectType:(NSString *)typeName {
+    return [_configuration classWithName:typeName] != nil;
+}
+
+- (SAClassDescription *)classDescriptionForObject:(NSObject *)object {
+    NSParameterAssert(object != nil);
+
+    Class aClass = [object class];
+    while (aClass != nil) {
+        SAClassDescription *classDescription = [_configuration classWithName:NSStringFromClass(aClass)];
+        if (classDescription) {
+            return classDescription;
+        }
+
+        aClass = [aClass superclass];
+    }
+
+    return nil;
+}
+
+@end

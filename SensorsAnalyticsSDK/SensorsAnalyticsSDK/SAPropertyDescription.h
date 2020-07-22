7c20887c2d59ddc8f59c4f38637e3134139d941c
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-22 22:21:11 +0000

    Updated Project

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAPropertyDescription.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAPropertyDescription.h
@@ -3,4 +3,13 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 雨晗 on 1/18/16.
-//  Copyr
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+/// Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class SAObjectSerializerContext;
+
+@interface SAPropertySelectorParameter

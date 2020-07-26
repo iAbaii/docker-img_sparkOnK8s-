af31d19a8b168e38949b731a7bf0fc4cf4bf3bc6
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-07-26 10:28:31 +0000

    Responsive backend for mobile devices #1027

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAPropertyDescription.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAPropertyDescription.m
@@ -2,4 +2,13 @@
 //  SAPropertyDescription.m
 //  SensorsAnalyticsSDK
 //
-//  Created by 雨晗 on 
+//  Created by 雨晗 on 1/18/16.
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+/// Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import "SALogger.h"
+#import "SAPropertyDescription.h"
+
+@implementation SAPropertySe

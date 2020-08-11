af35abefb83f90af543e43173035c462416afad3
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-08-11 16:02:11 +0000

    adjust colors

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAReachability.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAReachability.h
@@ -1,2 +1,9 @@
 /*
- Copyright
+ Copyright (C) 2016 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ Basic demonstration of how to use the SystemConfiguration Reachablity APIs.
+ */
+
+#import <Foundation/Foundation.h>

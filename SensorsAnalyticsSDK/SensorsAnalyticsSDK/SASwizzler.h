b1515b55a6c76e4ff1ee0d475d4335c185bf379e
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-08-25 13:07:14 +0000

    Update data.json

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SASwizzler.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SASwizzler.h
@@ -3,4 +3,13 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 雨晗 on 1/20/16
-//  Copyright (c) 2016年 
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+///  Created by Alex Hofsteede on 5/5/14.
+///  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Cast to turn things that are not ids into NSMapTable keys
+#de

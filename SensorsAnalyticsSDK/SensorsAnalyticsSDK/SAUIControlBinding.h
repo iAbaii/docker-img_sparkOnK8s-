9f67e89884f0e4a9ac8f0cc073f4e51f61b9575f
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-08-29 13:14:45 +0000

    default layout tests

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAUIControlBinding.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAUIControlBinding.h
@@ -2,3 +2,14 @@
 //  SAUIControlBinding.h
 //  SensorsAnalyticsSDK
 //
+//  Created by 雨晗 on 1/20/16
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAEventBinding.h"
+
+@interface SAUIControlBinding : SAEventBinding
+
+@proper

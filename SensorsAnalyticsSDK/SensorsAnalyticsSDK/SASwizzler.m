2131a4264cd3a3c9bcb2dfa939eb727213ad099a
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-08-29 07:12:37 +0000

    New commit, no message

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SASwizzler.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SASwizzler.m
@@ -174,3 +174,119 @@ static void (*sa_swizzledMethods_bool[MAX_BOOL_ARGS - MIN_BOOL_ARGS + 1])(id, SE
     
     uint numArgs = method_getNumberOfArguments(aMethod);
     if (numArgs < MIN_BOOL_ARGS || numArgs > MAX_BOOL_ARGS) {
+        [NSException raise:@"SwizzleBoolException" format:@"Cannot swizzle method with %d args", numArgs];
+    }
+    
+    IMP swizzledMethod = (IMP)sa_swizzledMethods_bool[numArgs - 3];
+    [SASwizzler swizzleSelector:aSelector onClass:aClass withBlock:aBlock andSwizzleMethod:swizzledMethod named:aName];
+}
+
++ (void)swizzleSelector:(SEL)aSelector
+                onClass:(Class)aClass
+              withBlock:(swizzleBlock)aBlock
+       andSwizzleMethod:(IMP)aSwizzleMethod
+                  named:(NSString *)aName {
+    Method aMethod = class_getInstanceMethod(aClass, aSelector);
+    if (!aMethod) {
+        [NSException raise:@"SwizzleException" format:@"Cannot find method for %@ on %@", NSStringFromSelector(aSelector), NSStringFromClass(aClass)];
+    }
+    
+    BOOL isLocal = [self isLocallyDefinedMethod:aMethod onClass:aClass];
+    SASwizzle *swizzle = [self swizzleForMethod:aMethod];
+    
+    if (isLocal) {
+        if (!swizzle) {
+            IMP originalMethod = method_getImplementation(aMethod);
+            
+            // Replace the local implementation of this method with the swizzled one
+            method_setImplementation(aMethod, aSwizzleMethod);
+            
+            // Create and add the swizzle
+            swizzle = [[SASwizzle alloc] initWithBlock:aBlock named:aName forClass:aClass selector:aSelector originalMethod:originalMethod];
+            [self setSwizzle:swizzle forMethod:aMethod];
+        } else {
+            [swizzle.blocks setObject:aBlock forKey:aName];
+        }
+    } else {
+        IMP originalMethod = swizzle ? swizzle.originalMethod : method_getImplementation(aMethod);
+        
+        // Add the swizzle as a new local method on the class.
+        if (!class_addMethod(aClass, aSelector, aSwizzleMethod, method_getTypeEncoding(aMethod))) {
+            [NSException raise:@"SwizzleException" format:@"Could not add swizzled for %@::%@, even though it didn't already exist locally", NSStringFromClass(aClass), NSStringFromSelector(aSelector)];
+        }
+        // Now re-get the Method, it should be the one we just added.
+        Method newMethod = class_getInstanceMethod(aClass, aSelector);
+        if (aMethod == newMethod) {
+            [NSException raise:@"SwizzleException" format:@"Newly added method for %@::%@ was the same as the old method", NSStringFromClass(aClass), NSStringFromSelector(aSelector)];
+        }
+        
+        SASwizzle *newSwizzle = [[SASwizzle alloc] initWithBlock:aBlock named:aName forClass:aClass selector:aSelector originalMethod:originalMethod];
+        [self setSwizzle:newSwizzle forMethod:newMethod];
+    }
+}
+
++ (void)unswizzleSelector:(SEL)aSelector onClass:(Class)aClass {
+    Method aMethod = class_getInstanceMethod(aClass, aSelector);
+    SASwizzle *swizzle = [self swizzleForMethod:aMethod];
+    if (swizzle) {
+        method_setImplementation(aMethod, swizzle.originalMethod);
+        [self removeSwizzleForMethod:aMethod];
+    }
+}
+
+/*
+ Remove the named swizzle from the given class/selector. If aName is nil, remove all
+ swizzles for this class/selector
+*/
++ (void)unswizzleSelector:(SEL)aSelector onClass:(Class)aClass named:(NSString *)aName {
+    Method aMethod = class_getInstanceMethod(aClass, aSelector);
+    SASwizzle *swizzle = [self swizzleForMethod:aMethod];
+    if (swizzle) {
+        if (aName) {
+            [swizzle.blocks removeObjectForKey:aName];
+        }
+        if (!aName || [swizzle.blocks count] == 0) {
+            method_setImplementation(aMethod, swizzle.originalMethod);
+            [self removeSwizzleForMethod:aMethod];
+        }
+    }
+}
+
+@end
+
+
+@implementation SASwizzle
+
+- (instancetype)init {
+    if ((self = [super init])) {
+        self.blocks = [NSMapTable mapTableWithKeyOptions:(NSPointerFunctionsStrongMemory | NSPointerFunctionsObjectPersonality)
+                                            valueOptions:(NSPointerFunctionsStrongMemory | NSPointerFunctionsObjectPointerPersonality)];
+    }
+    return self;
+}
+
+- (instancetype)initWithBlock:(swizzleBlock)aBlock
+              named:(NSString *)aName
+           forClass:(Class)aClass
+           selector:(SEL)aSelector
+     originalMethod:(IMP)aMethod {
+    if ((self = [self init])) {
+        self.class = aClass;
+        self.selector = aSelector;
+        self.originalMethod = aMethod;
+        [self.blocks setObject:aBlock forKey:aName];
+    }
+    return self;
+}
+
+- (NSString *)description {
+    NSString *descriptors = @"";
+    NSString *key;
+    NSEnumerator *keys = [self.blocks keyEnumerator];
+    while ((key = [keys nextObject])) {
+        descriptors = [descriptors stringByAppendingFormat:@"\t%@ : %@\n", key, [self.blocks objectForKey:key]];
+    }
+    return [NSString stringWithFormat:@"Swizzle on %@::%@ [\n%@]", NSStringFromClass(self.class), NSStringFromSelector(self.selector), descriptors];
+}
+
+@end
---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAUIControlBinding.h
@@ -0,0 +1,4 @@
+//
+//  SAUIControlBinding.h
+//  SensorsAnalyticsSDK
+//

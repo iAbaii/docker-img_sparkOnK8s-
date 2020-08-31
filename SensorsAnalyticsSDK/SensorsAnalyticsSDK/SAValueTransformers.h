48c9a0a1ce1cf32aa09376e24ec4868db6ed594e
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-08-31 15:10:26 +0000

    Add missing copyright headers.

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAValueTransformers.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAValueTransformers.h
@@ -3,4 +3,12 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 雨晗 on 1/20/16
-//  
+//  Copyright (c) 2016年 SensorsData. All rights reserved.
+//
+///  Created by Alex Hofsteede on 5/5/14.
+///  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAPassThroughValueTransfor

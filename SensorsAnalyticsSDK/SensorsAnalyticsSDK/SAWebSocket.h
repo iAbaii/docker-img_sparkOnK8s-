befa65b5a87bfe4ad3031892b84880d938b5d1f1
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-09-05 19:59:38 +0000

    yapf

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAWebSocket.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SAWebSocket.h
@@ -1,4 +1,11 @@
 //
 //  Copyright (c) 2016年 SensorsData. All rights reserved.
 //
-/// Copyright (c) 2014 Mixpanel. All
+/// Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+//   Portions Copyright 2012 Square Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You 

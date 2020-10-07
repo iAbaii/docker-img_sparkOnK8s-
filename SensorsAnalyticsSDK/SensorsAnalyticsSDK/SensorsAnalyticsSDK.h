c55b47fbdd81e7394be37e33f95c933c82721fa7
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-10-07 17:33:27 +0000

    Bye

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.h
@@ -173,3 +173,230 @@ typedef NS_OPTIONS(NSInteger, SensorsAnalyticsNetworkType) {
  * 它也提供了一个<code>SensorsAnalyticsPeople</code>类型的property，用来访问用户Profile相关的API。
  */
 @interface SensorsAnalyticsSDK : NSObject
+
+/**
+ * @property
+ *
+ * @abstract
+ * 对<code>SensorsAnalyticsPeople</code>这个API的访问接口
+ */
+@property (atomic, readonly, strong) SensorsAnalyticsPeople *people;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 获取用户的唯一用户标识
+ */
+@property (atomic, readonly, copy) NSString *distinctId;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 用户登录唯一标识符
+ */
+@property (atomic, readonly, copy) NSString *loginId;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 当App进入活跃状态时，是否从SensrosAnalytics获取新的可视化埋点配置
+ *
+ * @discussion
+ * 默认值为 YES。
+ */
+@property (atomic) BOOL checkForEventBindingsOnActive;
+
+/**
+ * @proeprty
+ *
+ * @abstract
+ * 当App进入后台时，是否执行flush将数据发送到SensrosAnalytics
+ *
+ * @discussion
+ * 默认值为 YES
+ */
+@property (atomic) BOOL flushBeforeEnterBackground;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 两次数据发送的最小时间间隔，单位毫秒
+ *
+ * @discussion
+ * 默认值为 15 * 1000 毫秒， 在每次调用track、trackSignUp以及profileSet等接口的时候，
+ * 都会检查如下条件，以判断是否向服务器上传数据:
+ * 1. 是否WIFI/3G/4G网络
+ * 2. 是否满足以下数据发送条件之一:
+ *   1) 与上次发送的时间间隔是否大于 flushInterval
+ *   2) 本地缓存日志数目是否达到 flushBulkSize
+ * 如果同时满足这两个条件，则向服务器发送一次数据；如果不满足，则把数据加入到队列中，等待下次检查时把整个队列的内容一并发送。
+ * 需要注意的是，为了避免占用过多存储，队列最多只缓存10000条数据。
+ */
+@property (atomic) UInt64 flushInterval;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 本地缓存的最大事件数目，当累积日志量达到阈值时发送数据
+ *
+ * @discussion
+ * 默认值为 100，在每次调用track、trackSignUp以及profileSet等接口的时候，都会检查如下条件，以判断是否向服务器上传数据:
+ * 1. 是否WIFI/3G/4G网络
+ * 2. 是否满足以下数据发送条件之一:
+ *   1) 与上次发送的时间间隔是否大于 flushInterval
+ *   2) 本地缓存日志数目是否达到 flushBulkSize
+ * 如果同时满足这两个条件，则向服务器发送一次数据；如果不满足，则把数据加入到队列中，等待下次检查时把整个队列的内容一并发送。
+ * 需要注意的是，为了避免占用过多存储，队列最多只缓存10000条数据。
+ */
+@property (atomic) UInt64 flushBulkSize;
+
+/**
+ * @property
+ *
+ * @abstract
+ * 可视化埋点中，UIWindow 对象。
+ *
+ * @discussion
+ * 该方法应在 SDK 初始化完成后立即调用
+ *
+ * 默认值为App 的 UIWindow 对象是 UIApplication 的 windows 列表中的 firstObject，若用户调用 UIWindow 的 makeKeyAndVisible 等方法，
+ * 改变了 windows 列表中各个对象的 windowLevel，会导致可视化埋点无法正常获取需要埋点的 UIWindow 对象。用户调用该借口，设置可视化埋点需要管理的
+ * UIWindow 对象
+ */
+@property (atomic) UIWindow *vtrackWindow;
+
+/**
+ * @abstract
+ * 根据传入的配置，初始化并返回一个<code>SensorsAnalyticsSDK</code>的单例
+ *
+ * @discussion
+ * 该方法会根据 <code>configureURL</code> 参数的 Url Path，自动计算可视化埋点配置系统的 Url。例如，若传入的 <code>configureURL</code> 为:
+ *     http://sa_host:8007/api/vtrack/config/iOS.conf
+ * 则会自动生成可视化埋点配置系统的 Url:
+ *     ws://sa_host:8007/api/ws
+ * 若用户私有环境中部署了 Sensors Analytics 系统，并修改了 Nginx 配置，则需要使用 SensorsAnalyticsSDK#sharedInstanceWithServerURL:andConfigureURL:andDebugMode 进行初始化。
+ *
+ * @param serverURL 收集事件的 URL
+ * @param configureURL 获取配置信息的 URL
+ * @param debugMode Sensors Analytics 的 Debug 模式
+ *
+ * @return 返回的单例
+ */
++ (SensorsAnalyticsSDK *)sharedInstanceWithServerURL:(NSString *)serverURL
+                                     andConfigureURL:(NSString *)configureURL
+                                        andDebugMode:(SensorsAnalyticsDebugMode)debugMode;
+
+/**
+ * @abstract
+ * 根据传入的配置，初始化并返回一个<code>SensorsAnalyticsSDK</code>的单例
+ *
+ * @param serverURL 收集事件的URL
+ * @param configureURL 获取配置信息的URL
+ * @param vtrackServerURL 可视化埋点配置系统的URL
+ * @param debugMode Sensors Analytics 的Debug模式
+ *
+ * @return 返回的单例
+ */
++ (SensorsAnalyticsSDK *)sharedInstanceWithServerURL:(NSString *)serverURL
+                                     andConfigureURL:(NSString *)configureURL
+                                  andVTrackServerURL:(nullable NSString *)vtrackServerURL
+                                        andDebugMode:(SensorsAnalyticsDebugMode)debugMode;
+
+/**
+ * @abstract
+ * 返回之前所初始化好的单例
+ *
+ * @discussion
+ * 调用这个方法之前，必须先调用<code>sharedInstanceWithServerURL</code>这个方法
+ *
+ * @return 返回的单例
+ */
++ (SensorsAnalyticsSDK *)sharedInstance;
+
+/**
+ * @abstract
+ * 允许 App 连接可视化埋点管理界面
+ *
+ * @discussion
+ * 调用这个方法，允许 App 连接可视化埋点管理界面并设置可视化埋点。建议用户只在 DEBUG 编译模式下，打开该选项。
+ *
+ */
+- (void)enableEditingVTrack;
+
+/**
+ * @abstract
+ * 将distinctId传递给当前的WebView
+ *
+ * @discussion
+ * 混合开发时,将distinctId传递给当前的WebView
+ *
+ * @param webView 当前WebView，支持<code>UIWebView</code>和<code>WKWebView</code>
+ *
+ * @return YES:SDK已进行处理，NO:SDK没有进行处理
+ */
+- (BOOL)showUpWebView:(id)webView WithRequest:(NSURLRequest *)request;
+
+/**
+ * @abstract
+ * 将distinctId传递给当前的WebView
+ *
+ * @discussion
+ * 混合开发时,将distinctId传递给当前的WebView
+ *
+ * @param webView 当前WebView，支持<code>UIWebView</code>和<code>WKWebView</code>
+ * @param request NSURLRequest
+ * @param propertyDict NSDictionary 自定义扩展属性
+ *
+ * @return YES:SDK已进行处理，NO:SDK没有进行处理
+ */
+- (BOOL)showUpWebView:(id)webView WithRequest:(NSURLRequest *)request andProperties:(nullable NSDictionary *)propertyDict;
+
+/**
+ * @abstract
+ * 设置本地缓存最多事件条数
+ *
+ * @discussion
+ * 默认为 10000 条事件
+ *
+ * @param maxCacheSize 本地缓存最多事件条数
+ */
+- (void)setMaxCacheSize:(UInt64)maxCacheSize;
+
+- (UInt64)getMaxCacheSize;
+
+/**
+ * @abstract
+ * 设置 flush 时网络发送策略
+ *
+ * @discussion
+ * 默认 3G、4G、WI-FI 环境下都会尝试 flush
+ *
+ * @param networkType SensorsAnalyticsNetworkType
+ */
+- (void)setFlushNetworkPolicy:(SensorsAnalyticsNetworkType)networkType;
+
+/**
+ * @abstract
+ * 登录，设置当前用户的loginId
+ *
+ * @param loginId 当前用户的loginId
+ */
+- (void)login:(NSString *)loginId;
+
+/**
+ * @abstract
+ * 注销，清空当前用户的loginId
+ *
+ */
+- (void)logout;
+
+/**
+ * @abstract
+ * 获取匿名id
+ *
+ * @return anonymousId 匿名id

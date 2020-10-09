38d93045bea982e29ae208859cbd299092275dc7
iAbaii <91178006+iAbaii@users.noreply.github.com>
2020-10-09 13:14:32 +0000

    Javadocs.

---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -0,0 +1,11 @@
+//  SensorsAnalyticsSDK.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 曹犟 on 15/7/1.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+
+#import <objc/runtime.h>
+#include <sys/sysctl.h>
+#include <stdlib.h>
+
+#import <CoreT

c4a158cadd5437ec23222323b1c5fd7320d8eafa
iAbaii <91178006+iAbaii@users.noreply.github.com>
2021-12-20 16:00:44 +0000

    Remove eslint

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -1631,4 +1631,8 @@ static SensorsAnalyticsSDK *sharedInstance = nil;
             SEL sharedManagerSelector = NSSelectorFromString(@"sharedManager");
             id sharedManager = ((id (*)(id, SEL))[ASIdentifierManagerClass methodForSelector:sharedManagerSelector])(ASIdentifierManagerClass, sharedManagerSelector);
             SEL advertisingIdentifierSelector = NSSelectorFromString(@"advertisingIdentifier");
-            NSUUID *uuid = ((NSUUID* (*)(id, SEL))[sharedManager methodForSelector:advertisingIdentifierSelector])(sharedManager, advert
+            NSUUID *uuid = ((NSUUID* (*)(id, SEL))[sharedManager methodForSelector:advertisingIdentifierSelector])(sharedManager, advertisingIdentifierSelector);
+            NSString *temp = [uuid UUIDString];
+            // 在 iOS 10.0 以后，当用户开启限制广告跟踪，advertisingIdentifier 的值将是全零
+            // 00000000-0000-0000-0000-000000000000
+            if (temp && ![temp 

f9daf9fcac4a59867cc1be698e31c96345fd70b0
iAbaii <91178006+iAbaii@users.noreply.github.com>
2022-09-03 14:22:43 +0000

    Trying

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIApplication+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIApplication+AutoTrack.m
@@ -2,4 +2,13 @@
 //  UIApplication+AutoTrack.m
 //  SensorsAnalyticsSDK
 //
-//  Created by
+//  Created by 王灼洲 on 17/3/22.
+//  Copyright (c) 2017年 SensorsData. All rights reserved.
+//
+
+#import "UIApplication+AutoTrack.h"
+#import "SALogger.h"
+#import "SensorsAnalyticsSDK.h"
+#import "AutoTrackUtils.h"
+
+@implementation UI

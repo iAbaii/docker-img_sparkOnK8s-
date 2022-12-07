c2c704480bcf798b4574fbe604e80dc57a94a6ee
iAbaii <91178006+iAbaii@users.noreply.github.com>
2022-12-07 22:59:15 +0000

    deploy: c7c44edd1af7ad633573c7b6e4dcd871a3f35900

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UICollectionView+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UICollectionView+AutoTrack.m
@@ -1,2 +1,10 @@
 //
-// 
+//  UICollectionView+SensorsAnalytics.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 王灼洲 on 17/3/22.
+//  Copyright © 2017年 SensorsData. All rights reserved.
+//
+
+#import "UICollectionView+AutoTrack.h"
+#import "SensorsAnalyticsSD

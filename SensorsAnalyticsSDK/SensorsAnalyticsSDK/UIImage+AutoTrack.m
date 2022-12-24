49a4b6941adf40366c18c6f3f7c71a181c5526ef
iAbaii <91178006+iAbaii@users.noreply.github.com>
2022-12-24 12:25:25 +0000

    Milestone version.

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIImage+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIImage+AutoTrack.m
@@ -1,14 +1,10 @@
 //
-//  UIImage.h
+//  UIImage.m
 //  SensorsAnalyticsSDK
 //
 //  Created by 王灼洲 on 2017/6/13.
 //  Copyright © 2017年 SensorsData. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-NS_ASSUME_NONNULL_BEGIN
-@interface UIImage (AutoTrack)
-
-@end
-NS_ASSUME_NONNULL_END
+#import "UIImage+AutoTrack.h"
+#import "SensorsAn

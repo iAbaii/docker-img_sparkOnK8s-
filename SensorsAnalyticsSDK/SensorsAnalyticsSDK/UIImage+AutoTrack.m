40893fc65fb7a2d81d9990c0ac94ab2cc00ef1ca
iAbaii <91178006+iAbaii@users.noreply.github.com>
2022-12-24 18:30:14 +0000

    hmm...

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIImage+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIImage+AutoTrack.m
@@ -14,4 +14,11 @@
 #import <objc/message.h>
 
 @implementation UIImage (AutoTrack)
-#ifndef SENSORS_ANALYTICS_DISABLE_AUTOTRACK_UIIMAGE_IMAGEN
+#ifndef SENSORS_ANALYTICS_DISABLE_AUTOTRACK_UIIMAGE_IMAGENAME
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        @try {
+            Class selfClass = object_getClass([self class]);
+            
+  

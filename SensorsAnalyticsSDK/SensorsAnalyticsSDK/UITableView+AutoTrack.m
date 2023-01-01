1e328880b1b8b961ccc5c777c08db43040820ba1
iAbaii <91178006+iAbaii@users.noreply.github.com>
2023-01-01 09:53:59 +0000

    fixed failing test

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UITableView+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UITableView+AutoTrack.m
@@ -4,4 +4,13 @@
 //  SensorsAnalyticsSDK
 //
 //  Created by 王灼洲 on 17/3/22.
-//  Copyr
+//  Copyright © 2017年 SensorsData. All rights reserved.
+//
+
+#import "UITableView+AutoTrack.h"
+#import "SensorsAnalyticsSDK.h"
+#import "SALogger.h"
+#import "SASwizzle.h"
+#import "AutoTrackUtils.h"
+#import <objc/runtime.h>
+#import <objc/mes

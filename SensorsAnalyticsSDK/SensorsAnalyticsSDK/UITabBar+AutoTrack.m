c1c3b80e4da8bd5acb50726f7c8161951e037612
iAbaii <91178006+iAbaii@users.noreply.github.com>
2023-01-01 07:50:46 +0000

    add stylesheet

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UITabBar+AutoTrack.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UITabBar+AutoTrack.m
@@ -58,3 +58,117 @@ void sa_uiTabBarDidSelectRowAtIndexPath(id self, SEL _cmd, id tabBar, UITabBarIt
         
         if (!tabBar) {
             return;
+        }
+        
+        UIView *view = (UIView *)tabBar;
+        if (!view) {
+            return;
+        }
+        
+        if (view.sensorsAnalyticsIgnoreView) {
+            return;
+        }
+        
+        NSMutableDictionary *properties = [[NSMutableDictionary alloc] init];
+        
+        [properties setValue:@"UITabBar" forKey:@"$element_type"];
+        
+        //ViewID
+        if (view.sensorsAnalyticsViewID != nil) {
+            [properties setValue:view.sensorsAnalyticsViewID forKey:@"$element_id"];
+        }
+        
+        UIViewController *viewController = [view viewController];
+        
+        if (viewController == nil ||
+            [@"UINavigationController" isEqualToString:NSStringFromClass([viewController class])]) {
+            viewController = [[SensorsAnalyticsSDK sharedInstance] currentViewController];
+        }
+        
+        if (viewController != nil) {
+            if ([[SensorsAnalyticsSDK sharedInstance] isViewControllerIgnored:viewController]) {
+                return;
+            }
+            
+            //获取 Controller 名称($screen_name)
+            NSString *screenName = NSStringFromClass([viewController class]);
+            [properties setValue:screenName forKey:@"$screen_name"];
+            
+            NSString *controllerTitle = viewController.navigationItem.title;
+            if (controllerTitle != nil) {
+                [properties setValue:viewController.navigationItem.title forKey:@"$title"];
+            } else {
+                @try {
+                    UIView *titleView = viewController.navigationItem.titleView;
+                    if (titleView != nil) {
+                        if (titleView.subviews.count > 0) {
+                            NSString *elementContent = [[NSString alloc] init];
+                            for (UIView *subView in [titleView subviews]) {
+                                if (subView) {
+                                    if (subView.sensorsAnalyticsIgnoreView) {
+                                        continue;
+                                    }
+                                    if ([subView isKindOfClass:[UIButton class]]) {
+                                        UIButton *button = (UIButton *)subView;
+                                        if ([button currentTitle] != nil && ![@"" isEqualToString:[button currentTitle]]) {
+                                            elementContent = [elementContent stringByAppendingString:[button currentTitle]];
+                                            elementContent = [elementContent stringByAppendingString:@"-"];
+                                        }
+                                    } else if ([subView isKindOfClass:[UILabel class]]) {
+                                        UILabel *label = (UILabel *)subView;
+                                        if (label.text != nil && ![@"" isEqualToString:label.text]) {
+                                            elementContent = [elementContent stringByAppendingString:label.text];
+                                            elementContent = [elementContent stringByAppendingString:@"-"];
+                                        }
+                                    }
+                                }
+                            }
+                            if (elementContent != nil && [elementContent length] > 0) {
+                                elementContent = [elementContent substringWithRange:NSMakeRange(0,[elementContent length] - 1)];
+                                [properties setValue:elementContent forKey:@"$title"];
+                            }
+                        }
+                    }
+                } @catch (NSException *exception) {
+                    SAError(@"%@: %@", self, exception);
+                }
+            }
+        }
+        
+        if (item) {
+            [properties setValue:item.title forKey:@"$element_content"];
+        }
+        
+        //View Properties
+        NSDictionary* propDict = view.sensorsAnalyticsViewProperties;
+        if (propDict != nil) {
+            [properties addEntriesFromDictionary:propDict];
+        }
+        
+        [[SensorsAnalyticsSDK sharedInstance] track:@"$AppClick" withProperties:properties];
+    } @catch (NSException *exception) {
+        SAError(@"%@ error: %@", self, exception);
+    }
+}
+
+- (void)sa_uiTabBarSetDelegate:(id<UITabBarDelegate>)delegate {
+    [self sa_uiTabBarSetDelegate:delegate];
+    
+    @try {
+        Class class = [delegate class];
+        //        static dispatch_once_t onceToken;
+        //        dispatch_once(&onceToken, ^{
+        if (class_addMethod(class, NSSelectorFromString(@"sa_uiTabBarDidSelectRowAtIndexPath"), (IMP)sa_uiTabBarDidSelectRowAtIndexPath, "v@:@@")) {
+            Method dis_originMethod = class_getInstanceMethod(class, NSSelectorFromString(@"sa_uiTabBarDidSelectRowAtIndexPath"));
+            Method dis_swizzledMethod = class_getInstanceMethod(class, @selector(tabBar:didSelectItem:));
+            method_exchangeImplementations(dis_originMethod, dis_swizzledMethod);
+        }
+        //        });
+    } @catch (NSException *exception) {
+        SAError(@"%@ error: %@", self, exception);
+    }
+}
+
+#endif
+
+@end
---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UITableView+AutoTrack.m
@@ -0,0 +1,7 @@
+//
+//
+//  UITableView+SensorsAnalytics.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 王灼洲 on 17/3/22.
+//  Copyr

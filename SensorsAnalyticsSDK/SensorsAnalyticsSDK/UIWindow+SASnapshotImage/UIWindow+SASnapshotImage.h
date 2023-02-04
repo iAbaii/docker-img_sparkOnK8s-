6732c439685c9490f5e01f735d87efb1d91269ce
iAbaii <91178006+iAbaii@users.noreply.github.com>
2023-02-04 11:05:10 +0000

    Updated configure.

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIWindow+SASnapshotImage/UIWindow+SASnapshotImage.h
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/UIWindow+SASnapshotImage/UIWindow+SASnapshotImage.h
@@ -1,2 +1,13 @@
 //
-//  UIWindow+Sna
+//  UIWindow+SnapshotImage.h
+//  SensorsAnalyticsSDK
+//
+//  Created by 杜晓星 on 2017/11/29.
+//  Copyright © 2017年 SensorsData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (SASnapshotImage)
+
+- (nullable UIImage *)sna
